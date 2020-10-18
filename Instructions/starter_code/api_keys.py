547dd855b06d72a91e39e8da3574fd81545d9fba
LCHoffman <69278034+LCHoffman@users.noreply.github.com>
2020-10-18 11:34:03 -0400

    Update api_keys.py
    Removed my API keys

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "81968e7e73ca220361d46f956fe08d17"
+weather_api_key = "YOUR KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyDcIOc6_mXg6gMmyyOYnHj9Vfvm6GasfGI"
+g_key = "YOUR KEY HERE"
